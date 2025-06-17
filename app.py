ecf56fa43af8c92b4a59c625348a7b8edc1b0653
sravan778899 <gajulasravan44@gmail.com>
2025-06-17 23:04:31 +0530

    45

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def download():
     return send_file(file_stream, as_attachment=True, download_name="art.txt", mimetype="text/plain")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
